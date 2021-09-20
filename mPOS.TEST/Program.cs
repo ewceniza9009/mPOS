be97992d5e323c042cd2193197b0f296ce4663ac
Erwin Wilson Ceniza <erwinwilsonceniza@outllook.com>
2021-09-20 13:58:59 +0800

    Clean up

--- mPOS.TEST/Program.cs
+++ mPOS.TEST/Program.cs
@@ -18,38 +18,6 @@ namespace mPOS.TEST
                 //    var uri = (@"http://localhost/posserver/MstCustomer/GetCustomers");
                 var uri = (@"http://localhost/posserver/MstUser/canLogin");
 
-
-                //    var filterParam = new MstUserFilter()
-                //    {
-                //        FullName = "Walk",
-                //        filterMethods = new FilterMethods()
-                //        {
-                //            Operations = new List<FilterOperation>()
-                //            {
-                //                new FilterOperation("FullName", Operation.Contains),
-                //            }
-                //        }
-                //    };
-
-                //    var paramContent = JsonConvert.SerializeObject(filterParam);
-                //    var buffer = System.Text.Encoding.UTF8.GetBytes(paramContent);
-                //    var byteContent = new ByteArrayContent(buffer);
-
-                //    byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-
-                //    var responseContent = await client.PostAsync(uri, byteContent);
-                //    var response = await responseContent.Content.ReadAsStringAsync();
-                //    var customers = JsonConvert.DeserializeObject<List<MstCustomer>>(response);
-
-                //    customers.ForEach(x =>
-                //    {
-                //        Console.WriteLine(x.Customer);
-                //    });
-                //}
-
-                //var response = await client.GetStringAsync($"{uri}?id={5520}");
-                //var result = JsonConvert.DeserializeObject<MstCustomer>(response.ToString());
-
                 var user = new MstUser()
                 {
                     UserName = "admin",
