ddb79c9dba89fbbcd85e3c88d2c5b7c72a0a4216
Erwin Wilson Ceniza <59189152+ewceniza9009@users.noreply.github.com>
2023-01-10 21:14:18 +0800

    Rearrange method

--- mPOSv2/Services/ISave.cs
+++ mPOSv2/Services/ISave.cs
@@ -3,22 +3,26 @@ using System.Threading.Tasks;
 
 namespace mPOSv2.Services
 {
-    public interface ISave
-    {
-        void Save(string filename, string contentType, MemoryStream stream);
-    }
     public interface IPrint
     {
         void Print(string fileName, Stream inputStream);
     }
+
+    public interface ISave
+    {
+        void Save(string filename, string contentType, MemoryStream stream);
+    }
+
     public interface ISaveWindowsPhone
     {
         Task Save(string filename, string contentType, MemoryStream stream);
     }
+
     public interface IAndroidVersionDependencyService
     {
         int GetAndroidVersion();
     }
+
     public interface IMailService
     {
         void ComposeMail(string fileName, string[] recipients, string subject, string messagebody, MemoryStream documentStream);
